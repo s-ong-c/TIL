000743d9f70ae47c2302676e051d230f8519efaa
s-ong-c <somony9292@gmail.com>
2019-07-09 23:51:10 +0900

    재배포

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 yarn build
 
 # navigate into the build output directory
-cd documents/.vuepress/dist
+cd docs/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
