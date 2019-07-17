36185038eee008e898a559c2352a27df354cb090
s-ong-c <somony9292@gmail.com>
2019-07-17 17:44:00 +0900

    component 수정

--- documents/.vuepress/components/List.vue
+++ documents/.vuepress/components/List.vue
@@ -8,7 +8,7 @@ export default {
 
 }
 </script>
-<style>
+<style scoped>
 @import "https://fonts.googleapis.com/css?family=Gugi";
 
 .infoContainer {
--- documents/.vuepress/components/Spinner.vue
+++ documents/.vuepress/components/Spinner.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 
-<script>
+<script scoped>
 export default {
   
   name: 'ClipLoader',
